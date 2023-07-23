b4ae5aad481101ef08b8df3ac800f63b67129cba
SaratChandra Vemuri <saratchandra_vemuri@epam.com>
2023-07-23 20:03:50 +0530

    final

--- SecondFile.js
+++ SecondFile.js
@@ -4,6 +4,13 @@ const kirak = () => {
     console.log('Hello this is my Second  func')
     console.log('hello')
     console.log('hjsbfjsdkjf')
+    console.log('dsfksdf')
+    console.log('Hello this is my Second  func')
+    console.log('hello')
+    console.log('Hello this is my Second  func')
+    console.log('hello')
+    console.log('hjsbfjsdkjf')
+    console.log('dsfksdf')
 }
 
 kirak();
