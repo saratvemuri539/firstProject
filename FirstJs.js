0b861b816d4052f43df81619ac813d55d2dc0ce5
SaratChandra Vemuri <saratchandra_vemuri@epam.com>
2023-07-23 20:07:48 +0530

    push

--- FirstJs.js
+++ FirstJs.js
@@ -1,5 +1,8 @@
 const fun = () => {
     console.log('Hello this is my first func')
+    console.log('Hello this is my first func')
+    console.log('Hello this is my first func')
+    console.log('Hello this is my first func')
 }
 
 fun();
